05f91a71288a4c4e2a758542959e07b6b1acc616
Matheus Alan <matheusalanpv@outlook.com>
2023-08-10 10:55:53 -0300

    Primeiro commit

---
+++ handler-exception/src/test/java/br/com/handler/exception/HandlerExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.handler.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlerExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
