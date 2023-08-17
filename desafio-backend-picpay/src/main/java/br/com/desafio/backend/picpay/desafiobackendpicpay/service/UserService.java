1dcd415480c399a43fe6e26e0c1bd5feb5bada19
Matheus Alan <matheusalanpv@outlook.com>
2023-08-17 17:05:48 -0300

    Criado camada de serviço para entidade User e criado método para validar transações

---
+++ desafio-backend-picpay/src/main/java/br/com/desafio/backend/picpay/desafiobackendpicpay/service/UserService.java
@@ -0,0 +1,27 @@
+package br.com.desafio.backend.picpay.desafiobackendpicpay.service;
+
+import br.com.desafio.backend.picpay.desafiobackendpicpay.domain.user.TypeUser;
+import br.com.desafio.backend.picpay.desafiobackendpicpay.domain.user.User;
+import br.com.desafio.backend.picpay.desafiobackendpicpay.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == TypeUser.MERCHANT) {
+            throw new Exception("Usuário do tipo MERCHANT não está autorizado a realizar transações.");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente.");
+        }
+    }
+
+}
