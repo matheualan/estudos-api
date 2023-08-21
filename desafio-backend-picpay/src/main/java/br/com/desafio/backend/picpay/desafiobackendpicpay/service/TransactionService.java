601dbecc6702ad244ae92e2daeeb2ac70bb71179
Matheus Alan <matheusalanpv@outlook.com>
2023-08-21 16:14:09 -0300

    Criado método para autorizar e criar transações com validações para enviador e recebedor.

--- desafio-backend-picpay/src/main/java/br/com/desafio/backend/picpay/desafiobackendpicpay/service/TransactionService.java
+++ desafio-backend-picpay/src/main/java/br/com/desafio/backend/picpay/desafiobackendpicpay/service/TransactionService.java
@@ -1,11 +1,19 @@
 package br.com.desafio.backend.picpay.desafiobackendpicpay.service;
 
 import br.com.desafio.backend.picpay.desafiobackendpicpay.domain.dto.TransactionDTO;
+import br.com.desafio.backend.picpay.desafiobackendpicpay.domain.transaction.Transaction;
 import br.com.desafio.backend.picpay.desafiobackendpicpay.domain.user.User;
 import br.com.desafio.backend.picpay.desafiobackendpicpay.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -16,17 +24,56 @@ public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
 
-    private final WebClient webClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
+    private final WebClient webClient;
+//  "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6"
     public TransactionService(WebClient.Builder webClientBuilder) {
         webClient = webClientBuilder.build();
     }
 
+//    método para criar transações
     public void createTransaction(TransactionDTO transactionDTO) throws Exception {
+//        Pega o usuário que enviará para fazer a validação
         User sender = userService.findUserById(transactionDTO.senderId());
+//        Pega o usuário que receberá para atualizar o balance
         User receiver = userService.findUserById(transactionDTO.senderId());
 
         userService.validateTransaction(sender, transactionDTO.value());
+
+        boolean isAuthorizedRestTemplate = authorizeTransactionRestTemplate(sender, transactionDTO.value());
+        boolean isAuthorizedWebClient = authorizeTransactionWebClient(sender, transactionDTO.value());
+        if (!isAuthorizedRestTemplate) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(transactionDTO.value());
+        transaction.setSender(sender);
+        transaction.setReceiver(receiver);
+
+    }
+
+    public boolean authorizeTransactionWebClient(User sender, BigDecimal value) {
+        Mono<String> message = webClient.get()
+                .uri("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        if (message.toString().equalsIgnoreCase("Autorizado")) {
+            return true;
+        } else return false;
+    }
+
+    public boolean authorizeTransactionRestTemplate(User sender, BigDecimal amount) {
+        ResponseEntity<Map> respostaAutorizacao =
+                restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
+
+        if (respostaAutorizacao.getStatusCode() == HttpStatus.OK) {
+            String message = (String) respostaAutorizacao.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
     }
 
 }
--- desafio-backend-picpay/src/main/java/br/com/desafio/backend/picpay/desafiobackendpicpay/service/UserService.java
+++ desafio-backend-picpay/src/main/java/br/com/desafio/backend/picpay/desafiobackendpicpay/service/UserService.java
@@ -15,14 +15,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+//    método para validar transações (cliente pode fazer trans. entre cliente e lojista  mas lojista não pode enviar pra ngm
     public void validateTransaction(User sender, BigDecimal amount) throws Exception {
         if (sender.getUserType() == TypeUser.MERCHANT) {
-            throw new Exception("Usuário do tipo MERCHANT não está autorizado a realizar transações.");
-        }
-
-        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Usuário do tipo MERCHANT(lojista) não está autorizado a realizar transações.");
+        } else if (sender.getBalance().compareTo(amount) < 0) {
             throw new Exception("Saldo insuficiente.");
         }
+//
+//        if (sender.getBalance().compareTo(amount) < 0) {
+//            throw new Exception("Saldo insuficiente.");
+//        }
     }
 
     public User findUserById(Integer id) {
