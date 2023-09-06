073802afb1b245c00f7e6bac14dc37bd466d8022
Matheus Alan <matheusalanpv@outlook.com>
2023-09-05 22:15:07 -0300

    Projeto iniciado. Realizado conexão com o banco de dados H2 por meio de um arquivo.yml

---
+++ controle-de-vendas/src/main/java/br/com/controlevendas/ControleDeVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVendasApplication.class, args);
+	}
+
+}
---
+++ controle-de-vendas/src/test/java/br/com/controlevendas/ControleDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
