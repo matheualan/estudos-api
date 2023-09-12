ea3d78e493183fe2c6f760e54dc18486c2439727
Matheus Alan <matheusalanpv@outlook.com>
2023-09-12 14:45:20 -0300

    Validações na classe AddressDTO para testar se está sendo validado corretamente quando gerar um objeto User que contem um Address

--- controle-de-vendas/src/main/java/br/com/controlevendas/dto/AddressDTO.java
+++ controle-de-vendas/src/main/java/br/com/controlevendas/dto/AddressDTO.java
@@ -2,6 +2,7 @@ package br.com.controlevendas.dto;
 
 import br.com.controlevendas.model.Address;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,11 +15,21 @@ import lombok.Setter;
 @JsonIgnoreProperties(value = {"userDTO"})
 public class AddressDTO {
 
+    @Size(min = 8, max = 8)
     private String cep;
+
+    @Size(min = 3, max = 50)
     private String logradouro;
+
     private String complemento;
+
+    @Size(min = 3, max = 50)
     private String bairro;
+
+    @Size(min = 3, max = 50)
     private String localidade;
+
+    @Size(min = 2, max = 2)
     private String uf;
 
     private UserDTO userDTO;
