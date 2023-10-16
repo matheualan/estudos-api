32239e4fc111cf4de7f1e2bf985e0f0b2775f474
Matheus Alan <matheusalanpv@outlook.com>
2023-10-16 16:47:41 -0300

    ...

--- controle-de-vendas/src/main/java/br/com/controlevendas/service/users/OrderService.java
+++ controle-de-vendas/src/main/java/br/com/controlevendas/service/users/OrderService.java
@@ -33,18 +33,26 @@ public class OrderService {
         User userByCpf = userService.encontreUsuarioPorCpf(cpf);
 
         Order order = new Order(userByCpf, quantity, price);
-//        Order order = new Order(quantity, price);
-//        order.setUser(userByCpf);
+////        Order order = new Order(quantity, price);
+////        order.setUser(userByCpf);
 
-        order.setTotalOrders(order.getTotalOrders() + 1);
-        order.setTotalPurchased(order.getTotalPurchased().add(price));
+//        order.setTotalOrders(order.getTotalOrders() + 1);
+//        order.setTotalPurchased(order.getTotalPurchased().add(price));
 
 //        for (int i = 0; i < userByCpf.getOrders().size(); i++) {
 //            userByCpf.getOrders().get(i).setTotalOrders(userByCpf.getOrders().get(i).getTotalOrders() + 1);
 //            userByCpf.getOrders().get(i).setTotalPurchased(userByCpf.getOrders().get(i).getTotalPurchased().add(price));
 //        }
 
-        for (Order o : order.getUser().getOrders()) {
+//        for (Order o : order.getUser().getOrders()) {
+//            o.setTotalOrders(o.getTotalOrders() + 1);
+//            o.setTotalPurchased(o.getTotalPurchased().add(price));
+//        }
+
+        order.setTotalOrders(1);
+        order.setTotalPurchased(price);
+
+        for (Order o : userByCpf.getOrders()) {
             o.setTotalOrders(o.getTotalOrders() + 1);
             o.setTotalPurchased(o.getTotalPurchased().add(price));
         }
@@ -53,14 +61,14 @@ public class OrderService {
 
         return new OrderDTO(order);
 
-//        UserDTO userByCpf = userService.findUserByCpf(cpf);
-//        User user = new User(userByCpf);
-//        UserOrderDTO userOrderDTO = new UserOrderDTO(user);
-//        OrderDTO orderDTO = new OrderDTO(quantity, price, userOrderDTO);
-//        Order order = new Order(orderDTO);
-//        order.setUser(userByCpf);
-//        orderRepository.save(order);
-//        return orderDTO;
+////        UserDTO userByCpf = userService.findUserByCpf(cpf);
+////        User user = new User(userByCpf);
+////        UserOrderDTO userOrderDTO = new UserOrderDTO(user);
+////        OrderDTO orderDTO = new OrderDTO(quantity, price, userOrderDTO);
+////        Order order = new Order(orderDTO);
+////        order.setUser(userByCpf);
+////        orderRepository.save(order);
+////        return orderDTO;
     }
 
     public List<OrderDTO> listAllOrders() {
