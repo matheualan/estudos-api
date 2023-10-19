9142e78050af45b05e47e494243e0e7aa3885592
Matheus Alan <matheusalanpv@outlook.com>
2023-10-18 21:25:11 -0300

    Aula 05 - Customizando o tema do Angular Material

--- crud-angular-spring/crud-angular/src/app/cursos/cursos-routing.module.ts
+++ crud-angular-spring/crud-angular/src/app/cursos/cursos-routing.module.ts
@@ -5,7 +5,7 @@ import { CursosComponent } from './cursos/cursos.component';
 //Aqui é onde vamos adicionar os components desse módulo
 
 const routes: Routes = [
-  { path: '', component: CursosComponent }
+  { path: '', component: CursosComponent } //path: '' indica que é para a própria pasta(módulo) cursos
 ];
 
 @NgModule({
