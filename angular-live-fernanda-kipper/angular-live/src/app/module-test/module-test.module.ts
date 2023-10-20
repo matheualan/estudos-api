ee27fbc381df91374ea0fc7a277ec673db639549
Matheus Alan <matheusalanpv@outlook.com>
2023-10-19 22:51:26 -0300

    ...

---
+++ angular-live-fernanda-kipper/angular-live/src/app/module-test/module-test.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModuleTestRoutingModule } from './module-test-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModuleTestRoutingModule
+  ]
+})
+export class ModuleTestModule { }
