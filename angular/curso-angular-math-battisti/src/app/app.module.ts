ce960207b76082c7964322a6c258b27a4dcb9f8e
Matheus Alan <matheusalanpv@outlook.com>
2023-11-08 10:58:36 -0300

    Aula 09 Curso Angular 13 Matheus Battisti

--- angular/curso-angular-math-battisti/src/app/app.module.ts
+++ angular/curso-angular-math-battisti/src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PrimeiroCompComponent } from './components/primeiro-comp/primeiro-comp.component';
-import { ParentDataComponent } from './components/parent-data/parent-data.component';
-import { DiretivasComponent } from './components/diretivas/diretivas.component';
+import { PrimeiroCompComponent } from './components/1primeiro-comp/primeiro-comp.component';
+import { ParentDataComponent } from './components/2parent-data/parent-data.component';
+import { DiretivasComponent } from './components/3diretivas/diretivas.component';
+import { IfRenderComponent } from './components/4if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroCompComponent,
     ParentDataComponent,
-    DiretivasComponent
+    DiretivasComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/curso-angular-math-battisti/src/app/components/4if-render/if-render.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  templateUrl: './if-render.component.html',
+  styleUrls: ['./if-render.component.css']
+})
+export class IfRenderComponent {
+
+  // usado no *ngIf fornecendo a condição para exibir ou não a tag desejada
+  boleano: boolean = true;
+
+  name: string = 'Matheus';
+
+  idade: string = '28';
+
+}
