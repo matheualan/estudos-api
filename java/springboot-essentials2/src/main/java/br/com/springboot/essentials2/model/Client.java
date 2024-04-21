022a0a9f4fca96b44b7602debad92c728db2e4bc
Matheus Alan <matheusalanpv@outlook.com>
2024-04-20 23:35:32 -0300

    Testando commit na branch local new-request-post para depois fazer o merge para branch remota main

--- java/springboot-essentials2/src/main/java/br/com/springboot/essentials2/model/Client.java
+++ java/springboot-essentials2/src/main/java/br/com/springboot/essentials2/model/Client.java
@@ -7,20 +7,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
 
     private Integer idClient;
 
     private String name;
-
-    public Client() {
-    }
-
-    public Client(Integer idClient, String name) {
-        this.name = name;
-        this.idClient = idClient;
-    }
 
 }
