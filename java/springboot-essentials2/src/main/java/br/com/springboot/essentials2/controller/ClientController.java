910465229c9e2858c91976feda39c5fe18b7ec42
Matheus Alan <matheusalanpv@outlook.com>
2024-04-29 19:58:53 -0300

    Study return

--- java/springboot-essentials2/src/main/java/br/com/springboot/essentials2/controller/ClientController.java
+++ java/springboot-essentials2/src/main/java/br/com/springboot/essentials2/controller/ClientController.java
@@ -26,38 +26,38 @@ public class ClientController {
 
     @PostMapping
     public ResponseEntity<Client> saveClient(@RequestBody Client client) {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: POST, Method: saveClient()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: POST - Method: saveClient()"));
         return ResponseEntity.status(HttpStatus.CREATED).body(clientService.saveClient(client));
     }
 
     @GetMapping(value = "/list")
     public List<Client> listClients() {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET, Method: listClients()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET - Method: listClients()"));
         return clientService.listAll();
     }
 
     @GetMapping(value = "/findClientByIndex/{index}")
     public ResponseEntity<Client> findClientByIndex(@PathVariable Integer index) {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET, Method: findClientByIndex()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET - Method: findClientByIndex()"));
         return ResponseEntity.status(HttpStatus.OK).body(clientService.findClientByIndex(index));
     }
 
     @GetMapping(value = "/findClientById/{id}")
     public ResponseEntity<Client> findClientById(@PathVariable Integer id) {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET, Method: findClientById()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: GET - Method: findClientById()"));
         return ResponseEntity.status(HttpStatus.OK).body(clientService.findClientById(id));
     }
 
     @DeleteMapping(value = "/deleteClientById/{id}")
     public ResponseEntity<Void> deleteClientById(@PathVariable Integer id) {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: DELETE, Method: deleteClientById()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: DELETE - Method: deleteClientById()"));
         clientService.deleteClientById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PutMapping(value = "/replaceClientById")
     public ResponseEntity<Void> replaceClientById(@RequestBody Client client) {
-        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: PUT, Method: replaceClientById()"));
+        log.info(dateUtil.dateFormatter(LocalDateTime.now()).concat(" Request: PUT - Method: replaceClientById()"));
         clientService.replaceClientById(client);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
