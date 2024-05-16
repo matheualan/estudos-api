8d18e399087194ac2f11fc6d584184553be74b8b
Matheus Alan <matheusalanpv@outlook.com>
2024-05-16 16:39:42 -0300

    Projeto controle de vendas criado. Criado primeira entidade Client

---
+++ java/sales-control/src/main/java/com/salescontrol/model/Client.java
@@ -0,0 +1,29 @@
+package com.salescontrol.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_clients")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idClient;
+    private String name;
+    private String cpf;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
