a0d54bc6f5cc642383cee8d1ba27228f7075e3a1
Matheus Alan <matheusalanpv@outlook.com>
2024-05-20 15:21:35 -0300

    Modelagem entidade Order

--- java/sales-control/src/main/java/com/salescontrol/model/Order.java
+++ java/sales-control/src/main/java/com/salescontrol/model/Order.java
@@ -1,4 +1,34 @@
 package com.salescontrol.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_orders")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idOrder;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Client client;
+
+    private Double quantity;
+
+    private BigDecimal value;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private final LocalDateTime orderDate = LocalDateTime.now();
+
 }
