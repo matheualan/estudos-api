821e9b554227a0f915f7c47a4ff9761755f3ce5e
Matheus Alan <matheusalanpv@outlook.com>
2024-06-27 01:17:59 -0300

    ...

--- java/sales-control/src/main/resources/import.sql
+++ java/sales-control/src/main/resources/import.sql
@@ -3,3 +3,8 @@
 --INSERT INTO tb_clients (name, cpf) VALUES ('ccc', '333');
 --INSERT INTO tb_clients (name, cpf) VALUES ('ddd', '444');
 --INSERT INTO tb_clients (name, cpf) VALUES ('eee', '555');
+
+--% significa uma sequencia de caracteres sendo letras ou numeros
+--SELECT id_client, name, cpf FROM tb_clients WHERE name LIKE 'M%';
+--SELECT name FROM tb_clients WHERE name LIKE '%o';
+--SELECT name FROM tb_clients WHERE name LIKE '%a%';
