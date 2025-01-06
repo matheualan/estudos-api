05721359bc930c7cc658ad0f24cd5f7885f67f55
Matheus Alan <matheusalanpv@outlook.com>
2025-01-05 21:54:15 -0300

    ...

--- java/java-code/src/main/transactionProject/service/Transaction.java
+++ java/java-code/src/main/transactionProject/service/Transaction.java
@@ -12,7 +12,6 @@ public class Transaction {
 
         Person personOne = new Person("Matheus", "123", new BigDecimal(50000));
         Person personTwo = new Person("Alan", "987" , new BigDecimal(30000));
-
         System.out.println(personOne);
         System.out.println(personTwo);
 
